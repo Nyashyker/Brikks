ed97e447b0f686d9c53c37b0f12e306f406d6f46
ivanpanchev <ivan.panchev@student.kdg.be>
2024-12-09 22:42:18 +0100

    added emptySave and EmptyPlayerSave

--- sql/CreateTablesTry.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS players (
-                                       player_name VARCHAR(10),
-                                       top_score INTEGER
-);
-
-INSERT INTO players (player_name, top_score)
-VALUES ('Martin', 1234);
