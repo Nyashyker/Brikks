efb08bd75decdab04b008c58a26e02654ccc40c1
rado0000 <radostin.lazov@student.kdg.be>
2024-12-09 19:25:45 +0100

    more-hello

--- src/src/be/kdg/integration/brikks_project/BonusScore.java
+++ src/src/be/kdg/integration/brikks_project/BonusScore.java
@@ -1,4 +1,48 @@
 package be.kdg.integration.brikks_project;
 
 public class BonusScore {
+    public static final byte MAXIMUM_SCALE = 14;
+    private final byte[] SCALLING_RATE;
+    private byte scale;
+
+
+    public BonusScore(PlayerSave save) {
+        this.SCALLING_RATE = generateScallingRate();
+        this.scale = 0;
+    }
+
+    public BonusScore(PlayerSave save, byte[] scallingRate, byte scale) {
+        this.SCALLING_RATE = scallingRate;
+        this.scale = scale;
+    }
+
+    private byte[] generateScallingRate() {
+        return new byte[] {0, 1, 3, 6, 10, 15, 21, 28, 36, 44, 51, 58, 64, 70, 75};
+    }
+
+    public byte[] getScallingRate() {
+        return SCALLING_RATE;
+    }
+
+    public byte getScale() {
+        return scale;
+    }
+
+    public void grow(byte amount) {
+        if (scale + amount <= MAXIMUM_SCALE) {
+            scale += amount;
+        } else {
+            scale = MAXIMUM_SCALE;
+        }
+    }
+
+
+    public short calculateFinal() {
+        short total = 0;
+        for (byte rate : SCALLING_RATE) {
+            total += rate * scale;
+        }
+        return total;
+    }
 }
+
