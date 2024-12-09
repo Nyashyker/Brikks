a6d53ace8ca74b5ea8a1cac9cefe0dad5c9a9bf5
Nyashyker <nyashyker@gmail.com>
2024-12-09 14:58:34 +0100

    simp-esent-classes-implemented

--- crs/src/be/simp/PlacedBlock.java
+++ crs/src/be/simp/PlacedBlock.java
@@ -1,4 +1,37 @@
 package be.simp;
 
-public class PlacedBlock {
+public class PlacedBlock extends Block {
+    protected final Position position;
+
+    public PlacedBlock(Block block) {
+        super(block);
+        this.position = new Position(0, 0);
+    }
+
+    public PlacedBlock(final Block block, final Position position) {
+        super(block);
+        this.position = position;
+    }
+
+
+    public Position setPlace() {
+        return this.position;
+    }
+
+    public void setPlace(final Position position) {
+        this.position.set(position);
+        this.position.set(position);
+    }
+
+    @Override
+    public Position[] getBlock() {
+        Position[] block = new Position[this.shape.length + 1];
+
+        block[0] = this.position;
+        for (int i = 1; i < Block.len; i++) {
+            block[i] = this.shape[i];
+        }
+
+        return block;
+    }
 }
