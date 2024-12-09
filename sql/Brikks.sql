863770131e325b91f64701de707c7e0950736bde
rado0000 <radostin.lazov@student.kdg.be>
2024-12-09 12:00:58 +0100

    r3r

--- sql/Brikks.sql
+++ sql/Brikks.sql
@@ -1,4 +1,4 @@
-CREATE TABLE players (
++CREATE TABLE players (
     player_id NUMERIC(65536) CONSTRAINT player_pk PRIMARY KEY CONSTRAINT player_nn NOT NULL,
     name VARCHAR(16) CONSTRAINT name_nn NOT NULL
 );
