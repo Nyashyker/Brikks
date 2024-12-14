38af982366a16a81a20606bff4ae8b3080513553
Nyashyker <nyashyker@gmail.com>
2024-12-14 20:28:22 +0100

    disconect-from-the-main-branch

--- DATA_AND_AI_5/04_POC_Create_player/Brikks.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE players (
-    player_id SERIAL CONSTRAINT player_pk PRIMARY KEY CONSTRAINT player_nn NOT NULL,
-    name VARCHAR(16) CONSTRAINT name_nn NOT NULL
-);
---
+++ full/src/MakeDatabase.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.SQLException;
+
+public class MakeDatabase {
+    public static void main(String[] args) {
+        try (
+                Connection con = DriverManager.getConnection("jdbc:postgresql:file:db/brikks",
+                        "BrikksPlacer",
+                        "placeTHEbrikks");
+                Statement stmt = con.createStatement();
+        ) {
+            stmt.executeUpdate("CREATE DATABASE IF NOT EXISTS brikks");
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS players (name VARCHAR(3), score INTEGER)");
+        } catch (SQLException sql_e) {
+            System.out.println(sql_e.getMessage());
+        }
+    }
+}
