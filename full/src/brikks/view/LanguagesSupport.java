e69789022d7729ecc00fed75ea36314be3db0b02
rado0000 <radostin.lazov@student.kdg.be>
2025-01-02 20:28:09 +0100

    превод
    (cherry picked from commit 4ee9627f2d068145d9171a3251c23e1bd8efc051)

--- full/src/brikks/view/LanguagesSupport.java
+++ full/src/brikks/view/LanguagesSupport.java
@@ -3,14 +3,14 @@ package brikks.view;
 public class LanguagesSupport {
     public static boolean isTrue(final String message) {
         return switch (prepareMessage(message)) {
-            case "true", "t", "yes", "y", "так", "т", "1" -> true;
+            case "вярно", "да", "в", "д", "true", "t", "yes", "y", "так", "т", "1" -> true;
             default -> false;
         };
     }
 
     public static boolean isFalse(final String message) {
         return switch (prepareMessage(message)) {
-            case "false", "f", "no", "n", "ні", "н", "0" -> true;
+            case "не", "false", "f", "no", "n", "ні", "н", "0" -> true;
             default -> false;
         };
     }
