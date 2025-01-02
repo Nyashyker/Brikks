78a722dd07c880d9006cc416a8fce7d629f6da96
rado0000 <radostin.lazov@student.kdg.be>
2025-01-02 14:55:56 +0100

    should-be-good

--- DATA_AND_AI_5/04_POC_Create_player/Brikks.sql
+++ DATA_AND_AI_5/04_POC_Create_player/Brikks.sql
@@ -1,4 +1,5 @@
-CREATE TABLE players (
-    player_id SERIAL CONSTRAINT player_pk PRIMARY KEY CONSTRAINT player_nn NOT NULL,
-    name VARCHAR(16) CONSTRAINT name_nn NOT NULL
+CREATE TABLE IF NOT EXISTS players (
+    player_id INTEGER CONSTRAINT player_pk PRIMARY KEY CONSTRAINT player_nn NOT NULL,
+    name VARCHAR(16) CONSTRAINT name_nn NOT NULL,
+    player_highscore NUMERIC(191) CONSTRAINT pl_highscore_nn NOT NULL
 );
