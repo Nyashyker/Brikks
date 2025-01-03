baf3747c0731dbd100c88d783ec206b6230883d5
Nyashyker <nyashyker@gmail.com>
2025-01-03 19:34:23 +0100

    create-tables-SMALLINT

--- sql/recreate-tables.sql
+++ sql/recreate-tables.sql
@@ -1,10 +1,48 @@
-DROP TABLE IF EXISTS saved_boards;
 DROP TABLE IF EXISTS players_games;
-DROP TABLE IF EXISTS saved_players_games;
+DROP TABLE IF EXISTS saved_boards;
 DROP TABLE IF EXISTS blocks;
+DROP TABLE IF EXISTS saved_players_games;
 DROP TABLE IF EXISTS players;
 DROP TABLE IF EXISTS games;
 DROP TABLE IF EXISTS saved_games;
+
+CREATE TABLE saved_games
+(
+    save_id     SERIAL
+        CONSTRAINT pk_save_save_id PRIMARY KEY,
+    turn        SMALLINT
+        CONSTRAINT nn_turn NOT NULL
+        CONSTRAINT ch_turn CHECK ( turn >= 0 AND turn < 4 ),
+    roll_column SMALLINT
+        CONSTRAINT nn_roll_column NOT NULL
+        CONSTRAINT ch_roll_column CHECK ( roll_column >= 0 AND roll_column < 4 ),
+    roll_row    SMALLINT
+        CONSTRAINT nn_roll_row NOT NULL
+        CONSTRAINT ch_roll_row CHECK ( roll_row >= 0 AND roll_row < 6 ),
+    die_column  SMALLINT
+        CONSTRAINT nn_die_column NOT NULL
+        CONSTRAINT ch_die_column CHECK ( die_column >= 0 AND die_column < 4 ),
+    die_row     SMALLINT
+        CONSTRAINT nn_die_row NOT NULL
+        CONSTRAINT ch_die_row CHECK ( die_row >= 0 AND die_row < 6 )
+);
+
+CREATE TABLE games
+(
+    game_id    SERIAL
+        CONSTRAINT pk_game_id PRIMARY KEY,
+    start_dt   TIMESTAMP
+        CONSTRAINT nn_start NOT NULL,
+    end_dt     TIMESTAMP,
+    difficulty SMALLINT
+        CONSTRAINT nn_difficulty NOT NULL
+        CONSTRAINT ch_difficulty CHECK ( difficulty >= 0 AND difficulty < 4 ),
+    duel       BOOLEAN
+        CONSTRAINT nn_duel NOT NULL,
+    save_id    INT
+        CONSTRAINT fk_save_game_id REFERENCES saved_games (save_id)
+            ON DELETE SET NULL
+);
 
 CREATE TABLE players
 (
@@ -20,50 +58,51 @@ CREATE TABLE saved_players_games
         CONSTRAINT pk_save_player_id PRIMARY KEY,
     plays        BOOLEAN
         CONSTRAINT nn_plays NOT NULL,
-    bombs        NUMERIC(4)
-        CONSTRAINT nn_bombs NOT NULL,
-    energy       NUMERIC(28)
-        CONSTRAINT nn_energy NOT NULL,
-    energy_left  NUMERIC(29)
-        CONSTRAINT nn_energy_left NOT NULL,
-    bonus_score  NUMERIC(15)
-        CONSTRAINT nn_bonus_score NOT NULL,
-    player_order NUMERIC(4)
-        CONSTRAINT nn_order NOT NULL
+    bombs        SMALLINT
+        CONSTRAINT nn_bombs NOT NULL
+        CONSTRAINT ch_bombs CHECK ( bombs >= 0 AND bombs < 4 ),
+    energy       SMALLINT
+        CONSTRAINT nn_energy NOT NULL
+        CONSTRAINT ch_energy CHECK ( energy >= 0 AND energy < 28 ),
+    energy_left  SMALLINT
+        CONSTRAINT nn_energy_left NOT NULL
+        CONSTRAINT ch_energy_left CHECK ( energy_left >= 0 AND energy_left <= 28 ),
+    bonus_score  SMALLINT
+        CONSTRAINT nn_bonus_score NOT NULL
+        CONSTRAINT ch_bonus_score CHECK ( bonus_score >= 0 AND bonus_score < 15 ),
+    player_order SMALLINT
+        CONSTRAINT nn_player_order NOT NULL
+        CONSTRAINT ch_player_order CHECK ( player_order >= 0 AND player_order < 4 )
 );
 
-CREATE TABLE saved_games
+CREATE TABLE blocks
 (
-    save_id     SERIAL
-        CONSTRAINT pk_save_save_id PRIMARY KEY,
-    turn        NUMERIC(4)
-        --        CONSTRAINT fk_turn REFERENCES saved_players_games (player_order)
---            ON DELETE CASCADE
-        CONSTRAINT nn_turn NOT NULL,
-    roll_column NUMERIC(4)
-        CONSTRAINT nn_roll_column NOT NULL,
-    roll_row    NUMERIC(6)
-        CONSTRAINT nn_roll_row NOT NULL,
-    die_column  NUMERIC(4)
-        CONSTRAINT nn_die_column NOT NULL,
-    die_row     NUMERIC(6)
-        CONSTRAINT nn_die_row NOT NULL
+    block        SMALLSERIAL
+        CONSTRAINT pk_block PRIMARY KEY
+        CONSTRAINT ch_block CHECK ( block >= 0 AND block < 25 ),
+    table_column SMALLINT
+        CONSTRAINT ch_table_column CHECK ( table_column >= 0 AND table_column < 4 ),
+    table_row    SMALLINT
+        CONSTRAINT ch_table_row CHECK ( table_row >= 0 AND table_row < 6 )
 );
 
-CREATE TABLE games
+CREATE TABLE saved_boards
 (
-    game_id    SERIAL
-        CONSTRAINT pk_game_id PRIMARY KEY,
-    start_dt   TIMESTAMP
-        CONSTRAINT nn_start NOT NULL,
-    end_dt     TIMESTAMP,
-    difficulty NUMERIC(4)
-        CONSTRAINT nn_difficulty NOT NULL,
-    duel       BOOLEAN
-        CONSTRAINT nn_duel NOT NULL,
-    save_id    INT
-        CONSTRAINT fk_save_id REFERENCES saved_games (save_id)
-            ON DELETE SET NULL
+    save_id      INT
+        CONSTRAINT fk_save_id REFERENCES saved_players_games (save_id)
+            ON DELETE CASCADE,
+    x            SMALLINT
+        CONSTRAINT ch_x CHECK ( x >= 0 AND x < 4 ),
+    y            SMALLINT
+        CONSTRAINT ch_y CHECK ( y >= 0 AND y < 6 ),
+    energy_bonus SMALLINT
+        CONSTRAINT nn_energy_bonus NOT NULL
+        CONSTRAINT ch_energy_bonus CHECK ( energy_bonus >= 0 AND energy_bonus < 6 ),
+    block        SMALLINT
+        CONSTRAINT fk_block REFERENCES blocks (block)
+            ON DELETE RESTRICT
+        CONSTRAINT ch_block CHECK ( block >= 0 AND block < 25 ),
+    CONSTRAINT pk_save_cell PRIMARY KEY (save_id, x, y)
 );
 
 CREATE TABLE players_games
@@ -75,32 +114,10 @@ CREATE TABLE players_games
         CONSTRAINT fk_player_id REFERENCES players (player_id)
             ON DELETE RESTRICT,
     save_id   INT
-        CONSTRAINT fk_save_id REFERENCES saved_players_games (save_id)
+        CONSTRAINT fk_save_player_games_id REFERENCES saved_players_games (save_id)
             ON DELETE SET NULL,
     duration  INTERVAL,
-    score     NUMERIC(191),
+    score     SMALLINT
+        CONSTRAINT ch_score CHECK ( score >= 0 AND score <= 191 ),
     CONSTRAINT pk_player_game PRIMARY KEY (game_id, player_id)
-);
-
-CREATE TABLE blocks
-(
-    block        NUMERIC(25)
-        CONSTRAINT pk_block PRIMARY KEY,
-    table_column NUMERIC(4),
-    table_row    NUMERIC(6)
-);
-
-CREATE TABLE saved_boards
-(
-    save_id      INT
-        CONSTRAINT fk_save_id REFERENCES saved_players_games (save_id)
-            ON DELETE CASCADE,
-    x            NUMERIC(4),
-    y            NUMERIC(6),
-    energy_bonus NUMERIC(6)
-        CONSTRAINT nn_energy_bonus NOT NULL,
-    block        NUMERIC(25)
-        CONSTRAINT fk_block REFERENCES blocks (block)
-            ON DELETE RESTRICT,
-    CONSTRAINT pk_save_cell PRIMARY KEY (save_id, x, y)
 );
