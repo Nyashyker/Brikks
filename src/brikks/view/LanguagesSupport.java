f51f5ec9ac26cc1257e459325d324aba81cd9617
Nyashyker <nyashyker@gmail.com>
2025-05-28 20:44:36 +0200

    LSupport-Nederlands

--- src/brikks/view/LanguagesSupport.java
+++ src/brikks/view/LanguagesSupport.java
@@ -3,14 +3,14 @@ package brikks.view;
 public class LanguagesSupport {
     public static boolean isTrue(final String message) {
         return switch (prepareMessage(message)) {
-            case "вярно", "да", "в", "д", "true", "t", "yes", "y", "так", "т", "1" -> true;
+            case "ja", "j", "вярно", "да", "в", "д", "true", "t", "yes", "y", "так", "т", "1" -> true;
             default -> false;
         };
     }
 
     public static boolean isFalse(final String message) {
         return switch (prepareMessage(message)) {
-            case "не", "false", "f", "no", "n", "ні", "н", "0" -> true;
+            case "nee", "не", "false", "f", "no", "n", "ні", "н", "0" -> true;
             default -> false;
         };
     }
